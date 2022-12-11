11b8c282be6392c2806f87263d2ea8073cb32bb7
ziondayoo <adetunjitemidayo26@gmail.com>
2022-12-11 08:37:17 +0100

    Added Data

---
+++ ExamManagementAPI/Data/AppDataContext.cs
@@ -0,0 +1,16 @@
+﻿using ExamManagementAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamManagementAPI.Data
+{
+    public class AppDataContext:DbContext
+    {
+        public AppDataContext(DbContextOptions<AppDataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Exam> Exams { get; set; }
+        public DbSet<TheoryQuestion> TheoryQuestions { get; set; }
+        public DbSet<ObjectiveQuestion> ObjectiveQuestions  { get; set; }
+    }
+}
