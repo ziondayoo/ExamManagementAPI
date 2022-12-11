b840d7b7dba575fd8f1e234a5d7c799b8edb1499
ziondayoo <adetunjitemidayo26@gmail.com>
2022-12-11 08:43:25 +0100

    Added Exam Controller

---
+++ ExamManagementAPI/Controllers/ExamController.cs
@@ -0,0 +1,63 @@
+﻿using ExamManagementAPI.Models;
+using ExamManagementAPI.Models.DTOs;
+using ExamManagementAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ExamManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamController : ControllerBase
+    {
+        private readonly IExamService _examService;
+
+        public ExamController(IExamService examService)
+        {
+            _examService = examService;
+        }
+
+        [HttpPost]
+        [Route("create-exam")]
+        public async Task<IActionResult> CreateExam(CreateExamDTO exam)
+        {
+            var examToAdd = new Exam() { ExamName = exam.ExamName };
+            var result = await _examService.CreateExam(examToAdd);
+            return result ? Ok(exam) : BadRequest("exam could not be created");
+        }
+        [HttpPost]
+        [Route("add-theory-question/{examId}")]
+        public async Task<IActionResult> AddTheoryQuestion([FromBody] AddTheoryQuestionDTO question, string examId)
+        {
+            var exam = await _examService.GetExamById(examId);
+            var questionToAdd = new TheoryQuestion() { Question = question.Question, QuestionNumber = question.QuestionNumber };
+            exam?.TheoryQuestions?.Add(questionToAdd);
+            var result = await _examService.UpdateExam(exam);
+            return result ? Ok("question added successfully") : BadRequest("could not add question");
+        }
+
+        [HttpPost]
+        [Route("add-objective-question/{examId}")]
+        public async Task<IActionResult> AddObjectiveQuestion([FromBody] AddObjectiveQuestionDTO question, string examId)
+        {
+            var exam = await _examService.GetExamById(examId);
+            var questionToAdd = new ObjectiveQuestion() { QuestionNumber = question.QuestionNumber, Question = question.Question, QuestionOptions = question.QuestionOptions };
+
+
+            exam?.ObjectiveQuestions?.Add(questionToAdd);
+            var result = await _examService.UpdateExam(exam);
+            return result ? Ok("question added successfully") : BadRequest("could not add question");
+
+        }
+
+        [HttpGet]
+        [Route("get-all-exams")]
+        public async Task<IActionResult> GetAllExams()
+        {
+            var result = await _examService.GetAllExams();
+
+            return Ok(result);
+        }
+    }
+}
--- ExamManagementAPI/Program.cs
+++ ExamManagementAPI/Program.cs
@@ -1,3 +1,10 @@
+using ExamManagementAPI.Data;
+using ExamManagementAPI.Services.Implementations;
+using ExamManagementAPI.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +13,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IExamService, ExamService>();
 
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDataContext>(option => option.UseSqlServer(connectionString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
